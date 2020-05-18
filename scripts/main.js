374b25b80a2b53535e302384e2eb2a91f70dddb6
jinostrozam <bladimirosky@gmail.com>
2020-05-17 23:15:12 -0400

    daada

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,24 @@ miImage.onclick = function () {
       miImage.setAttribute('src', 'images/perro-chico.jpg');
     }
 }
+
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector('h1');
+
+function estableceNombreUsuario() {
+    var miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Todos los perritos se van al cielo, ' +miNombre;
+}
+
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    var nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Todos los perritos se van al cielo,' + nombreAlmacenado
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
