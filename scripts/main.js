85241141c3dd6767bec50c76ae8fcd33007eeb33
jinostrozam <bladimirosky@gmail.com>
2020-05-17 21:44:08 -0400

    asdaf

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
-var miTitulo = document.querySelector('h1');
-miTitulo.innerHTML = 'Hello world!';
+// var miTitulo = document.querySelector('h1');
+// miTitulo.innerHTML = 'Hello world!';
+
+var miImage = document.querySelector('img');
+
+miImage.onclick = function () {
+    var miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/perro-chico.jpg') {
+      miImage.setAttribute('src','images/cachorro.jpg');
+    } else {
+      miImage.setAttribute('src', 'images/perro-chico.jpg');
+    }
+}
